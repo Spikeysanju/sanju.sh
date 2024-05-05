bbe8aaef89d2c8a22e7f676e7c373abfe467baa5
SpikeySanju <spikeysanju98@gmail.com>
2024-05-05 10:14:39 +0530

    fix darkmode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,9 @@
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
+		  fontFamily: {
+      		uncut: ["UncutSans", "sans-serif"],
+    },
 		extend: {},
 	},
 	plugins: [require("@tailwindcss/typography")],
