1ccc318be9291dbf6adf4022ea726575149a6bd2
SpikeySanju <spikeysanju98@gmail.com>
2024-05-05 09:41:48 +0530

    fix import on mdx

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss from "@astrojs/rss";
+import { SITE_DESCRIPTION, SITE_TITLE } from '@data/index';
 import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
 	const posts = await getCollection("writing");
