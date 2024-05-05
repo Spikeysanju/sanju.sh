d9c2932b1c4d3bcd32724804139b46bca6ecdbd9
SpikeySanju <spikeysanju98@gmail.com>
2024-05-05 15:49:54 +0530

    chore: Update ships-item.astro to use Dots component for ship count display

--- src/components/misc/index.ts
+++ src/components/misc/index.ts
@@ -1,4 +1,5 @@
 export { default as Analytics } from "./analyticss.astro";
 export { default as Header } from "./headers.astro";
 export { default as HeaderLink } from "./header-link.astro";
-export { default as Layout } from "./formatted-date.astro";
+export { default as FormattedDate } from "./formatted-date.astro";
+export { default as Dots } from "./dots.astro";
