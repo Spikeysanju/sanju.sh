0d96683054d4f6d3f3dba4f24873e870b0217621
SpikeySanju <spikeysanju98@gmail.com>
2024-05-11 12:14:27 +0530

    feat: Include thoughts and ships collections in RSS feed
    This commit modifies the rss.xml.js file to include the "thoughts" and "ships" collections in the RSS feed. The code now fetches the collections and adds their items to the RSS feed generation. Each item is assigned the correct link path based on its collection.
    
    Co-authored-by: [spikeysanju]

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,25 +3,11 @@ import { SITE_DESCRIPTION, SITE_TITLE } from '@data/index';
 import { getCollection } from "astro:content";
 
 export async function GET(context) {
-	const posts = await getCollection("writing");
-	const thoughts = await getCollection("thought");
-	const ships = await getCollection("ship");
-
-	// thoughts are in /thoughts/slug and ships are in /ships/slug
-	// so we need to add the correct path to the link
-
-
-
-	// return rss({
-	// 	title: SITE_TITLE,
-	// 	description: SITE_DESCRIPTION,
-	// 	site: context.site,
-	// 	items: posts.map((post) => ({
-	// 		...post.data,
-	// 		link: `/writings/${post.slug}/`,
-	// 	})),
-	// });
-
+	const [ posts , thoughts, ships ] = await Promise.all([
+		getCollection("writing"),
+		getCollection("thought"),
+		getCollection("ship"),
+	]);
 
 	return rss({
 		title: SITE_TITLE,
