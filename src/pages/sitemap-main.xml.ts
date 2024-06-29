b1c803cd8e8ba3c4a9b7d8d4df0e1c457a56ae6f
SpikeySanju <spikeysanju98@gmail.com>
2024-06-29 08:14:47 +0530

    add sitemaps

---
+++ src/pages/sitemap-main.xml.ts
@@ -0,0 +1,43 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async () => {
+  
+  // Generate the sitemap
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${new URL('/', import.meta.env.SITE).href}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+      </url>
+
+      <url>
+        <loc>${new URL('/writings', import.meta.env.SITE).href}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+      </url>
+
+      <url>
+        <loc>${new URL('/thoughts', import.meta.env.SITE).href}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+      </url>
+
+      <url>
+        <loc>${new URL('/ships', import.meta.env.SITE).href}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+      </url>
+    </urlset>
+  `;
+	
+    return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+    },
+  });
+}
