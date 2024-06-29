7d97c7b3a00dbb4e213a27aab534b288d7f74824
SpikeySanju <spikeysanju98@gmail.com>
2024-06-29 08:15:23 +0530

    Update robots.txt to use correct sitemap URL

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -4,7 +4,7 @@ const robotsTxt = `
 User-agent: *
 Allow: /
 
-Sitemap: ${new URL('sitemap-index.xml', import.meta.env.SITE).href}
+Sitemap: ${new URL('sitemap.xml', import.meta.env.SITE).href}
 `.trim();
 
 export const GET: APIRoute = () => {
