124fbaa98b6ed978dd7c730577d4bc8aacb3b3ed
SpikeySanju <spikeysanju98@gmail.com>
2024-06-29 07:43:54 +0530

    Update Chinese tea cup description and image source

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,3 +1,4 @@
 export { default as WritingLayout } from "./writing-layout.astro";
 export { default as ShipsLayout } from "./ships-layout.astro";
 export { default as ThoughtsLayout } from "./thoughts-layout.astro";
+export {default as RootLayout} from "./root-layout.astro";
