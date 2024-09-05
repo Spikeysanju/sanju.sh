b2cfa20bb081e14590f54027ad3ce16517b0bd78
SpikeySanju <spikeysanju98@gmail.com>
2024-09-06 00:57:59 +0530

    chore: Remove unnecessary analytics tracking script

--- src/components/misc/index.ts
+++ src/components/misc/index.ts
@@ -1,4 +1,3 @@
-export { default as Analytics } from "./analyticss.astro";
 export { default as Header } from "./headers.astro";
 export { default as HeaderLink } from "./header-link.astro";
 export { default as FormattedDate } from "./formatted-date.astro";
