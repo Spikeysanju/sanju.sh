fa9dff2ae2b496950fd1d2ae16bf9bb60c4c6682
SpikeySanju <spikeysanju98@gmail.com>
2024-09-07 14:28:01 +0530

    feat: Add scroll progress component to pages
    Add the scroll progress component to the pages in the `src/pages` directory. This component displays a progress bar indicating the user's scroll position on the page. It helps users visualize their progress and navigate back to the top of the page with a click.
    
    The scroll progress component is added to the following pages:
    - `src/pages/ships/[...slug].astro`
    - `src/pages/thoughts/[...slug].astro`
    - `src/pages/writings/[...slug].astro`
    
    This enhancement improves the user experience by providing a visual indicator of their progress while scrolling through the content.

--- src/components/misc/index.ts
+++ src/components/misc/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from "./headers.astro";
 export { default as HeaderLink } from "./header-link.astro";
 export { default as FormattedDate } from "./formatted-date.astro";
 export { default as Dots } from "./dots.astro";
+export {default as ScrollProgress} from "./scroll-progress.astro";
