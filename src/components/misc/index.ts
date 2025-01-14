4e5a2351daf406be7b926f567504741770940a38
SpikeySanju <spikeysanju98@gmail.com>
2025-01-14 11:40:40 +0530

    add: dark mode

--- src/components/misc/index.ts
+++ src/components/misc/index.ts
@@ -2,4 +2,5 @@ export { default as Header } from "./headers.astro";
 export { default as HeaderLink } from "./header-link.astro";
 export { default as FormattedDate } from "./formatted-date.astro";
 export { default as Dots } from "./dots.astro";
-export {default as ScrollProgress} from "./scroll-progress.astro";
+export { default as ScrollProgress } from "./scroll-progress.astro";
+export { default as ThemeSwitcher } from "./theme-switcher.astro";
